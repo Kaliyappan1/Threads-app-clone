5f52333684156ac5a2c5a31a00bf20c7951b4cf8
kaliyappan <yappank17@gmail.com>
2024-08-03 20:14:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is Threads app clone project
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { Flex, Image, useColorMode } from "@chakra-ui/react"
+
+
+function Header() {
+   const {colorMode, toggleColorMode} = useColorMode()
+    
+  return <Flex justifyContent={"center"} mt={6} mb="12">
+    <Image
+    cursor={"pointer"}
+    alt="logo"
+    w={6}
+    onClick={toggleColorMode}
+    src={colorMode === "dark" ? "/light-logo.svg":"/dark-logo.svg"}
+    />
+
+  </Flex>
+}
+
+export default Header
