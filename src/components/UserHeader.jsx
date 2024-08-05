8e015716f30de336cbbe65c98cc0e999b546dece
kaliyappan <yappank17@gmail.com>
2024-08-05 12:03:33 +0530

    header and userpost and like and comend added

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,16 +1,36 @@
-import { Avatar, Box, Flex, Link, Menu, MenuButton, MenuItem, MenuList, Portal, Text, VStack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  Text,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
 
 function UserHeader() {
+  const toast = useToast();
+
   const copyURL = () => {
     const currentURL = window.location.href;
     navigator.clipboard.writeText(currentURL).then(() => {
-      console.log("URL copied to clipboard");
-    })
-    
-  }
+      toast({
+        title: "Link Copied",
+
+        status: "success",
+        duration: 400,
+        isClosable: true,
+      });
+    });
+  };
 
   return (
     <VStack gap={4} alignItems={"start"}>
@@ -37,32 +57,53 @@ function UserHeader() {
         </Box>
       </Flex>
 
-      <Text>
-        Co-founder, executive chairman and CEO of Meta Platforms.
-      </Text>
+      <Text>Co-founder, executive chairman and CEO of Meta Platforms.</Text>
       <Flex w={"full"} justifyContent={"space-between"}>
         <Flex gap={2} alignItems={"center"}>
           <Text color={"gray.light"}>3.2k followers</Text>
           <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
           <Link color={"gray.light"}>instagram.com</Link>
         </Flex>
-      <Flex>
-        <Box className="icon-container">
-          <BsInstagram size={24} cursor={"pointer"} />
-        </Box>
-        <Box className="icon-container">
-          <Menu>
-            <MenuButton>
-          <CgMoreO size={24} cursor={"pointer"} />          
-            </MenuButton>
-            <Portal>
-              <MenuList bg={"gray.dark"}>
-                <MenuItem bg={"gray.dark"} onClick={copyURL}>Copy Link</MenuItem>
-              </MenuList>
-            </Portal>
-          </Menu>
-        </Box>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList >
+                  <MenuItem  onClick={copyURL}>
+                    Copy Link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
       </Flex>
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Threads</Text>
+        </Flex>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid gray"}
+          color={"gray.light"}
+          pb="3"
+          justifyContent={"center"}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Replies</Text>
+        </Flex>
       </Flex>
     </VStack>
   );
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,10 +1,12 @@
 import UserHeader from "../components/UserHeader"
+import UserPost from "../components/UserPost"
 
 function UserPage() {
   return (
 
   <>
   <UserHeader/>
+  <UserPost/>
   </>
   )
 }
