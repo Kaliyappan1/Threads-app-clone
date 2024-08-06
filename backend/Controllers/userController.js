f75ddf3167d8ad9c6ceba0863f3ea792c755858f
kaliyappan <yappank17@gmail.com>
2024-08-06 19:40:04 +0530

    update user and check self follow deney and self profile only update other wise dont update

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -84,7 +84,7 @@ const followUnFollowUser = async(req, res) => {
         const userToModify = await User.findById(id);
         const currentUser = await User.findById(req.user._id)
 
-        if( id === req.user._id) return res.status(400).json({ message : "you cannot follow/unfollow yourself"});
+        if( id === req.user._id.toString()) return res.status(400).json({ message : "you cannot follow/unfollow yourself"});
 
         if(!userToModify || !currentUser) return res.status(400).json({message: "User not found"});
 
@@ -108,4 +108,40 @@ const followUnFollowUser = async(req, res) => {
         console.log("Error in followunfollow ", err.message);
       }
 }
-export { signupUser, loginUser, logoutUser, followUnFollowUser };
+
+const updateUser = async(req, res) => {
+    const {name, email, username, password, profilePic, bio} = req.body;
+    const userId = req.user._id;
+    try {
+        let user = await User.findById(userId)
+        if (!user) {
+            return res.status(400).json({ message: "user not found"})
+        }
+
+        if (req.params.id !== userId.toString()) {
+          return res.status(400).json({ message: "You cannot update other user's profile"});
+        }
+
+        if (password) {
+            const salt = await bcrypt.getSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+
+        user = await user.save();
+
+        res.status(200).json({message: "profile update successfully", user})
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log("Error in followunfollow ", err.message);
+    }
+}
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser };
