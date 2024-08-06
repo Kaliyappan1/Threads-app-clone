f059a3e337b2dd5edd2ffb4a6ced7fcb686d904e
kaliyappan <yappank17@gmail.com>
2024-08-06 16:38:33 +0530

    schema and routes and controllers added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,7 +25,7 @@ const userSchema = mongoose.Schema({
         type: String,
         default: "",
     },
-    followsers: {
+    followers: {
         type: [String],
         default: [],
     },
@@ -41,6 +41,6 @@ const userSchema = mongoose.Schema({
     timestamps: true  ,
 });
 
-const User = mongoose.model('User, userSchema');
+const User = mongoose.model('User', userSchema);
 
 export default User;
