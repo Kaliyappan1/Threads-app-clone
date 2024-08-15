20f0eb06ac4937a6001e355a5301a2664c4f7755
kaliyappan <yappank17@gmail.com>
2024-08-15 15:56:32 +0530

    env file module added and all pages working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- server: {
-  port: 3000,
-  // Get rid of the CORS error
-  proxy: {
-    "/api": {
-      target: "http://localhost:5000",
-      changeOrigin: true,
-      secure: false,
-    }
-  }
- }
-
-})
+  server: {
+    port: 3000,
+    // Get rid of the CORS error
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
